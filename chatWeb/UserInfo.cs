cca23dbf731126d5720fb6d5db87ab9be4201ada
xulong <2289063896@qq.com>
2023-06-10 22:31:33 +0800

    提交代码

---
+++ chatWeb/UserInfo.cs
@@ -0,0 +1,12 @@
+﻿using SqlSugar;
+
+namespace SignalRWebApp.Models
+{
+    public class UserInfo
+    {
+        [SugarColumn(IsPrimaryKey = true)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ chatWeb/UserToFriend.cs
@@ -0,0 +1,12 @@
+﻿using SqlSugar;
+
+namespace SignalRWebApp.Models
+{
+    public class UserToFriend
+    {
+        [SugarColumn(IsPrimaryKey = true)]
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string FriendId  { get; set; }
+    }
+}
